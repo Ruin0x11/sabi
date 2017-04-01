eaac59bfda9b6504a34cc471312a43a64e942021
Ian Pickering <ipickering2@gmail.com>
2017-04-01 13:45:32 -0500

    Property test changes

--- src/stats/mod.rs
+++ src/stats/mod.rs
@@ -2,6 +2,8 @@ use std::collections::HashMap;
 use std::collections::hash_map::Entry::*;
 use self::PropErr::*;
 
+pub use self::PropName::*;
+
 // TEMP: Experiment with different stat architectures and see what works.
 //  - Static/baked in
 //  - Key/value pairs
@@ -13,22 +15,30 @@ enum Prop {
     Num(i32),
 }
 
+// This can be refactored to have type checking later.
+// For now, Is[...] indicates bool, plural indicates array, [...]Val indicates numeric.
+#[derive(Eq, PartialEq, Hash, Clone, Debug)]
+pub enum PropName {
+    Happiness,
+    Sadness,
+}
+
 #[derive(Debug)]
 enum PropErr {
     NoSuchKey,
     WrongType,
 }
 
+/// Arbitrary key-value properties. Use when extending things that don't fit
+/// into the baked-in structs.
 struct Properties {
-    props: HashMap<String, Prop>,
+    props: HashMap<PropName, Prop>,
 }
 
-// The issues with this system are threefold:
+// The issues with this system are twofold:
 //  - Keys can have arbitrary types, while typically a certain kind of property
 //    always has a set type.
 //  - One has to remember the correct type for a key when using 'get'.
-//  - It's possible to look for a key that shouldn't be there. There ought to be
-//    a list of valid keys.
 impl Properties {
     pub fn new() -> Self {
         Properties {
@@ -36,18 +46,18 @@ impl Properties {
         }
     }
 
-    pub fn get<T>(&self, key: &str) -> Result<T, PropErr>
-        where Properties: GetProp<T> {
+    pub fn get<T>(&self, key: PropName) -> Result<T, PropErr>
+        where Properties: GetProp<T, PropKey=PropName> {
         self.get_prop(key)
     }
 
-    pub fn set<T>(&mut self, key: &str, val: T) -> Result<(), PropErr>
-        where Properties: GetProp<T> {
+    pub fn set<T>(&mut self, key: PropName, val: T) -> Result<(), PropErr>
+        where Properties: GetProp<T, PropKey=PropName> {
         self.set_prop(key, val)
     }
 
-    pub fn remove(&mut self, key: &str) -> () {
-        self.props.remove(key);
+    pub fn remove(&mut self, key: PropName) -> () {
+        self.props.remove(&key);
     }
 }
 
@@ -60,16 +70,18 @@ impl Properties {
 // won't be an issue.
 // TEMP: This is just a dirty hack.
 trait GetProp<T> {
-    fn get_prop(&self, key: &str) -> Result<T, PropErr>;
-    fn set_prop(&mut self, key: &str, val: T) -> Result<(), PropErr>;
-    fn remove_prop(&mut self, key: &str) -> Result<(), PropErr>;
+    type PropKey;
+    fn get_prop(&self, key: Self::PropKey) -> Result<T, PropErr>;
+    fn set_prop(&mut self, key: Self::PropKey, val: T) -> Result<(), PropErr>;
+    fn remove_prop(&mut self, key: Self::PropKey) -> Result<(), PropErr>;
 }
 
 macro_rules! make_get_set {
     ($ty:ty, $path:path) => {
         impl GetProp<$ty> for Properties {
-            fn get_prop(&self, key: &str) -> Result<$ty, PropErr> {
-                if let Some(prop) = self.props.get(key) {
+            type PropKey = PropName;
+            fn get_prop(&self, key: PropName) -> Result<$ty, PropErr> {
+                if let Some(prop) = self.props.get(&key) {
                     if let $path(val) = *prop {
                         Ok(val)
                     } else {
@@ -80,8 +92,8 @@ macro_rules! make_get_set {
                 }
             }
 
-            fn set_prop(&mut self, key: &str, val: $ty) -> Result<(), PropErr> {
-                match self.props.entry(key.to_string()) {
+            fn set_prop(&mut self, key: PropName, val: $ty) -> Result<(), PropErr> {
+                match self.props.entry(key) {
                     Occupied(mut v) => {
                         if let $path(..) = *v.get() {
                             *v.get_mut() = $path(val);
@@ -98,8 +110,8 @@ macro_rules! make_get_set {
             }
 
             // This could return T in the future, given a type annotation.
-            fn remove_prop(&mut self, key: &str) -> Result<(), PropErr> {
-                match self.props.remove(key) {
+            fn remove_prop(&mut self, key: PropName) -> Result<(), PropErr> {
+                match self.props.remove(&key) {
                     Some(_) => Ok(()),
                     None      => Err(NoSuchKey)
                 }
@@ -118,34 +130,34 @@ mod tests {
     #[test]
     fn test_set() {
         let mut props = Properties::new();
-        let res = props.set("wobbly", true);
+        let res = props.set(Happiness, 32);
         assert!(res.is_ok());
 
-        let wobbly = props.get::<bool>("wobbly").unwrap();
-        assert_eq!(wobbly, true);
+        let hp = props.get::<i32>(Happiness).unwrap();
+        assert_eq!(hp, 32);
 
-        let flimsy = props.get::<bool>("flimsy");
-        assert!(flimsy.is_err());
+        let tp = props.get::<i32>(Sadness);
+        assert!(tp.is_err());
 
-        let res = props.set("wobbly", 123);
+        let res = props.set(Happiness, false);
         assert!(res.is_err());
 
-        let res = props.set("wobbly", false);
+        let res = props.set(Happiness, 128);
         assert!(res.is_ok());
 
-        // let res = props.set(IsWobbly, false);
-        // let res = props.get(IsWobbly).unwrap();
+        let hp = props.get::<i32>(Happiness).unwrap();
+        assert_eq!(hp, 128);
     }
 
     #[test]
     fn test_remove() {
         let mut props = Properties::new();
-        let res = props.set("wobbly", true);
+        let res = props.set(Happiness, 128);
         assert!(res.is_ok());
 
-        props.remove("wobbly");
+        props.remove(Happiness);
 
-        let res = props.get::<bool>("wobbly");
+        let res = props.get::<i32>(Happiness);
         assert!(res.is_err());
     }
 }
